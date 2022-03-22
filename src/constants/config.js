96b6bb472f1ff8f70585d61578e8133c03b2d03c
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2022-03-22 14:46:28 +0300

    Added a config file for more general customization of site text and values

---
+++ src/constants/config.js
@@ -0,0 +1,11 @@
+const config = {
+  "greeting": "Hello, I am",
+  "name": "Mehmet",
+  "headline": "I create scalable and maintainable solutions",
+  "introduction": "lorem ipsum",
+  "linkedIn": "https://www.linkedin.com/in/alpermehmetozdemir/",
+  "github": "https://github.com/AlperMehmetOzdemir"
+
+}
+
+export default config;
